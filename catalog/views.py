3efe32aa5ab22d960515b0b89210b9a9f3ec5388
129lovely <129lovely@gmail.com>
2020-01-11 05:19:52 +0900

    tutorial 7

--- catalog/views.py
+++ catalog/views.py
@@ -8,6 +8,10 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+
+    # 방문자수 세션 처리
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     
     num_genres_contain_romance = Genre.objects.filter(name__contains='로맨스').count()
     
@@ -16,7 +20,8 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_genres_contain_romance': num_genres_contain_romance
+        'num_genres_contain_romance': num_genres_contain_romance,
+        'num_visits': num_visits,
     }
     
     return render(request, 'index.html', context=context)
